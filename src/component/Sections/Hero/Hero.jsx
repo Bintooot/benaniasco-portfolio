14acff4add6ec97f8e90590576753c7508296ea6
RamDiv <142648607+Bintooot@users.noreply.github.com>
2025-05-28 12:13:09 +0800

    changing the file location

--- src/component/Sections/Hero/Hero.jsx
+++ src/component/Sections/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = (props) => {
   
   const handleDownload = () => {
     const link = document.createElement("a");
-    link.href = "/public/files/CV.pdf";
+    link.href = "/files/CV.pdf";
     link.download = "CV.pdf";
     document.body.appendChild(link);
     link.click();
