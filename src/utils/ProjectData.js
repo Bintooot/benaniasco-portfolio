0387af6b67cb198283e14b8ae707149e997d4eab
RamDiv <142648607+Bintooot@users.noreply.github.com>
2025-06-09 22:15:32 +0800

    adding new data

--- src/utils/ProjectData.js
+++ src/utils/ProjectData.js
@@ -2,32 +2,44 @@ import RMS from "../assets/RMS.png";
 import AppController from "../assets/App-Controller.png";
 import Velienne from "../assets/Velienne.png";
 import DevFinder from "../assets/DevFinder.png";
+import PortfolioV2 from "../assets/Portfolio-v2.png";
 
 export const projectData = [
   {
     id: 1,
+    name: "Portfolio V2 | Ben Raymond Aniasco",
+    description:
+      "This is my second portfolio website, showcasing my skills and projects. It features a modern design, responsive layout, and interactive elements to enhance user experience. The site highlights my work in web development and provides links to my GitHub and live projects.",
+    image: PortfolioV2,
+    time: "Jun 2025 - Jun 2025",
+    techStack: ["HTML", "CSS", "JavaScript", "React"],
+    github: "https://github.com/Bintooot/aniascoben-portfolio",
+    live: "https://bintooot.github.io/aniascoben-portfolio/",
+  },
+  {
+    id: 2,
     name: "DevFinder",
     description:
       "DevFinder is a responsive web app that allows users to search for GitHub profiles and view key information including avatar, bio, repositories, followers, and more.",
     image: DevFinder,
-    time: "Jun 3, 2025 - Jun 4, 2025",
+    time: "Jun 2025 - Jun 2025",
     techStack: ["HTML", "CSS", "JavaScript"],
     github: "https://github.com/Bintooot/DevFinder",
     live: "https://bintooot.github.io/DevFinder/",
   },
   {
-    id: 2,
+    id: 3,
     name: "Velienne - Landing Page",
     description:
       "A visually appealing landing page designed to showcase the Velienne Restaurant. It features a modern design, highlights the restaurant's offerings, and provides an easy-to-navigate interface for users to explore the menu and make reservations.",
     image: Velienne,
-    time: "May 30, 2025 - Jun 2, 2025",
+    time: "May 2025 - Jun 2025",
     techStack: ["HTML", "CSS", "JavaScript", "React"],
     github: "https://github.com/Bintooot/velienne",
     live: "https://bintooot.github.io/velienne/",
   },
   {
-    id: 3,
+    id: 4,
     name: "IoT Based Mobile Incubator Monitoring & Controller",
     description:
       "It is a mobile application that allows users to monitor and control the incubator. It also allows users to view the status of the incubator and the eggs inside it.",
@@ -38,7 +50,7 @@ export const projectData = [
     live: "#",
   },
   {
-    id: 4,
+    id: 5,
     name: "Request Management System",
     description:
       "This project focuses on managing user requests for chicks, specifically designed for government use. The system allows users to register, submit requests, track the status of their requests, and receive the chicks once approved. This web-based platform streamlines the process for individuals seeking chicks for their farms, helping the government efficiently manage and process these requests.",
